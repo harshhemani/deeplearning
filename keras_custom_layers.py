350a58b48f53f4a29fa9674d417733ab015d9fb5
harshhemani <harsh_science777@yahoo.com>
2015-09-17 17:02:48 +0530

    Create keras_custom_layers.py
    Right now just a regular encoder, but i'll make it a denoising autoencoder soon

---
+++ keras_custom_layers.py
@@ -0,0 +1,81 @@
+import theano
+import theano.tensor as T
+from keras.layers.core import Layer, Activation
+from keras.utils.theano_utils import shared_zeros
+from keras import initializations, activations, regularizers, constraints
+
+theano.config.exception_verbosity = 'high'
+theano.config.optimizer = 'fast_compile'
+
+class DAE(Layer):
+    '''
+        Denoising AutoEncoder
+        Well, right now just a regular encoder, but i'll make it a denoising autoencoder soon 
+    '''
+    def __init__(self, input_dim, output_dim, init='glorot_uniform', activation='linear', weights=None, name=None,
+                 W_regularizer=None, b_regularizer=None, activity_regularizer=None,
+                 W_constraint=None, b_constraint=None):
+
+        super(DAE, self).__init__()
+        self.init = initializations.get(init)
+        self.activation = activations.get(activation)
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+
+        self.input = T.matrix()
+        self.W = self.init((self.input_dim, self.output_dim))
+        self.b = shared_zeros((self.output_dim))
+        self.bT = shared_zeros((self.input_dim))
+
+        self.params = [self.W, self.b, self.bT]
+
+        self.regularizers = []
+        self.W_regularizer = regularizers.get(W_regularizer)
+        if self.W_regularizer:
+            self.W_regularizer.set_param(self.W)
+            self.regularizers.append(self.W_regularizer)
+
+        self.b_regularizer = regularizers.get(b_regularizer)
+        if self.b_regularizer:
+            self.b_regularizer.set_param(self.b)
+            self.regularizers.append(self.b_regularizer)
+
+        self.activity_regularizer = regularizers.get(activity_regularizer)
+        if self.activity_regularizer:
+            self.activity_regularizer.set_layer(self)
+            self.regularizers.append(self.activity_regularizer)
+
+        self.W_constraint = constraints.get(W_constraint)
+        self.b_constraint = constraints.get(b_constraint)
+        self.constraints = [self.W_constraint, self.b_constraint]
+
+        if weights is not None:
+            self.set_weights(weights)
+
+        if name is not None:
+            self.set_name(name)
+
+    def set_name(self, name):
+        self.W.name = '%s_W' % name
+        self.b.name = '%s_b' % name
+        self.bT.name = '%s_bT' % name
+
+    def get_output(self, train=False):
+        X = self.get_input(train)
+        if train:
+            output = self.activation(T.dot(self.activation(T.dot(X, self.W) + self.b), self.W.T) + self.bT)
+        else:
+            output = self.activation(T.dot(X, self.W) + self.b)
+        return output
+
+    def get_config(self):
+        return {"name": self.__class__.__name__,
+                "input_dim": self.input_dim,
+                "output_dim": self.output_dim,
+                "init": self.init.__name__,
+                "activation": self.activation.__name__,
+                "W_regularizer": self.W_regularizer.get_config() if self.W_regularizer else None,
+                "b_regularizer": self.b_regularizer.get_config() if self.b_regularizer else None,
+                "activity_regularizer": self.activity_regularizer.get_config() if self.activity_regularizer else None,
+                "W_constraint": self.W_constraint.get_config() if self.W_constraint else None,
+                "b_constraint": self.b_constraint.get_config() if self.b_constraint else None}
